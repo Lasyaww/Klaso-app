5d041a44fe963902ace78c361ebf79f8184ad756
Klaso Dev <dev@klaso.app>
2026-08-27 22:07:33 +0530

    Remove dummy test files and generator scripts

--- dummy_tests.js
+++
@@ -1,8 +0,0 @@
-const test = require('node:test');
-const assert = require('node:assert');
-
-for (let i = 1; i <= 300; i++) {
-    test(`fake test case ${String(i).padStart(3, '0')}`, () => {
-        assert.strictEqual(1, 1);
-    });
-}
